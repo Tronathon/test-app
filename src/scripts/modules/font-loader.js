dbfe0bdf7ec98580d4178ffe3d0b2413d242edb1
Ryan <ryan@nixondesign.com>
2018-04-11 09:19:02 +0100

    Switch to tiny-cookie package

--- src/scripts/modules/font-loader.js
+++ src/scripts/modules/font-loader.js
@@ -2,19 +2,19 @@
  * Async font loading via FontFaceObserver
  */
 
-import Cookies from 'js-cookie';
 import FontFaceObserver from 'fontfaceobserver';
+import { getCookie, setCookie } from 'tiny-cookie';
 
 const typefaces = {};
 
 export default function init() {
-	if (Cookies.get('fonts-loaded')) {
+	if (getCookie('fonts-loaded')) {
 		return false;
 	}
 
 	loadFonts().then(function() {
 		document.documentElement.classList.add('fonts-loaded');
-		Cookies.set('fonts-loaded', '1', { expires: 7, secure: true });
+		setCookie('fonts-loaded', '1', { expires: 7, secure: true });
 	});
 };
 
