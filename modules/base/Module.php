2b0240faaccbf93b72e4c6e81d3e069a8f5b5291
Ryan <ryan@nixondesign.com>
2018-06-18 16:18:57 +0100

    Remove comment

--- modules/base/Module.php
+++ modules/base/Module.php
@@ -1,26 +1,4 @@
 <?php
-namespace modules;
-
-use Craft;
-
-/**
- * Custom module class.
- *
- * This class will be available throughout the system via:
- * `Craft::$app->getModule('my-module')`.
- *
- * You can change its module ID ("my-module") to something else from
- * config/app.php.
- *
- * If you want the module to get loaded on every request, uncomment this line
- * in config/app.php:
- *
- *     'bootstrap' => ['my-module']
- *
- * Learn more about Yii module development in Yii's documentation:
- * http://www.yiiframework.com/doc-2.0/guide-structure-modules.html
- */
-
 namespace modules\base;
 
 use modules\base\twigextensions\TwigExtension;
