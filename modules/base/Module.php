300644e35f90064c5915c0cdd07feb55c44a5183
Ryan <ryan@nixondesign.com>
2018-07-11 12:05:45 +0100

    Improve image macro

--- modules/base/Module.php
+++ modules/base/Module.php
@@ -1,6 +1,7 @@
 <?php
 namespace modules\base;
 
+use modules\base\services\ImageService;
 use modules\base\twigextensions\TwigExtension;
 
 use Craft;
@@ -16,11 +17,20 @@ class Module extends \yii\base\Module
     {
         Craft::setAlias('@modules/base', $this->getBasePath());
 
+        parent::init();
+
         if (Craft::$app->getRequest()->getIsSiteRequest()) {
             // Add in our Twig extension
             Craft::$app->getView()->registerTwigExtension(new TwigExtension());
         }
 
-        parent::init();
+        Event::on(CraftVariable::class, CraftVariable::EVENT_INIT, function(Event $e) {
+            /** @var CraftVariable $variable */
+            $variable = $e->sender;
+
+            // Attach a service:
+            $variable->set('image', ImageService::class);
+        });
+
     }
 }
---
+++ modules/base/services/ImageService.php
@@ -0,0 +1,50 @@
+<?php
+namespace modules\base\services;
+
+use craft\elements\Asset;
+use yii\base\Component;
+
+class ImageService extends Component
+{
+    /**
+     * Creates a string that can be used in an img elements srcset attribute
+     *
+     * @param Asset
+     * @param array|null The desired widths
+     * @return string
+     */
+    function srcset(Asset $asset, array $widths = [], $ratio = null, array $transforms = [])
+    {
+        $candidates = [];
+
+        if (count($widths) == 0) {
+            return null;
+        }
+
+        foreach ($widths as &$width) {
+            $url = $asset->getUrl(array_merge([
+                'width' => $width,
+                'height' => $width * $ratio,
+            ], array_filter($transforms)));
+
+            $candidates[$width] = $url;
+        }
+
+        return $this->createSrcsetString($candidates);
+    }
+
+    /**
+     * Creates a string that can be used in the srcset attribute.
+     *
+     * @param array $array
+     * @return string
+     */
+    private function createSrcsetString(array $array = [])
+    {
+        array_walk($array, function(&$value, $key) {
+            $value = "{$value} {$key}w";
+        });
+
+        return implode(', ', $array);
+    }
+}
