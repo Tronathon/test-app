c4c883241916e918bf9ee1a1f7e88ef9ceeae4a4
Anthony Mooney <anthony@nixondesign.com>
2019-04-29 17:26:33 +0100

    implement flickr api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue';
-import axios from 'axios';
 import Home from './components/Home';
-import Entry from './components/Entry';
 
-var app = new Vue({
-    el: '#app',
-    data () {
-        return info
-    },
-    components: {
-		Home,
-        Entry
-	},
-	created () {
-		let url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=XazQc7UyC2JxBbz9bcED1D7LMa4i8Q9pRQHTbOA3'
-		axios.get(url)
-		.then(function(response){
-			console.log(response.data)
-		})
-	}
-});
+Vue.config.productionTip = true;
+
+new Vue({
+  render: h => h(Home),
+}).$mount('#app');
+
+// var app = new Vue({
+// 	el: '#app',
+// 	props: ['image'],
+// 	components: {
+// 		Home
+// 	},
+// 	data () {
+// 		return {
+// 			image
+
+// 		}
+
+// 	}
+// })
+
